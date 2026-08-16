542f42c36eb6913f98463f686d80bc82da6d0fd4
sagarshelake14 <sagarshelake0014@gmail.com>
2026-08-16 20:47:29 +0530

    project setup and mongoDb setup & connections

---
+++ config/dbConfig.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const dns = require('node:dns');
+
+// 1. Force Node.js to use Google and Cloudflare DNS servers first
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+// 2. Now initiate the database connection
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log("MongoDB connection successful");
+});
+
+connection.on('error', (err) => {
+    console.log("MongoDB connection failed");
+});
+
+module.exports = mongoose;
